f0744223d39e39ac01e731f2a4aa9819e65e0700
xiyori <foma@shipilov.ru>
2021-03-03 17:52:43 +0300

    Webp augmentation

--- dl_modules/augmentations.py
+++ dl_modules/augmentations.py
@@ -1,5 +1,7 @@
 import cv2
 import albumentations as albu
+import albumentations.augmentations.transforms.ImageCompression.ImageCompressionType \
+        as CompressionType
 
 
 def get_training_augmentation(crop_size: int):
@@ -36,7 +38,10 @@ def get_input_image_augmentation():
             [
                 albu.IAAAdditiveGaussianNoise(scale=(0.005 * 255, 0.005 * 255), p=1),
                 albu.GaussNoise(var_limit=(5.0, 20.0), p=1),
-                albu.ImageCompression(quality_lower=98, p=1)
+                albu.ImageCompression(quality_lower=98,
+                                      compression_type=CompressionType.JPEG, p=1),
+                albu.ImageCompression(quality_lower=98,
+                                      compression_type=CompressionType.WEBP, p=1)
             ],
             p=1
         )
