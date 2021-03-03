6fa7820283259b26da9e9d644de8390f2aa7aa0d
xiyori <foma@shipilov.ru>
2021-03-03 08:57:23 +0300

    Cool augmentations

--- dl_modules/dataset.py
+++ dl_modules/dataset.py
@@ -120,13 +120,30 @@ def get_validation_augmentation(crop_size: int):
     ])
 
 
-def get_input_image_augmentation(blur_limit: int):
+def get_input_image_augmentation():
     return albu.Compose([
-        albu.Blur(blur_limit=blur_limit, p=0.2),
-        albu.IAAAdditiveGaussianNoise(p=0.2),
-        albu.IAASharpen(p=0.2),
-        albu.Downscale(scale_min=0.5, scale_max=0.5, interpolation=cv2.INTER_AREA, p=0.5),
-        albu.ImageCompression(quality_lower=95, p=0.7)
+        albu.OneOf(
+            [
+                albu.Compose([
+                    albu.Blur(blur_limit=2, p=1),
+                    albu.IAASharpen(alpha=(0.5, 0.8), lightness=(0.9, 1.0), p=1)
+                ], p=1),
+                albu.Compose([
+                    albu.GaussianBlur(blur_limit=4, p=1),
+                    albu.IAASharpen(alpha=(0.5, 0.8), lightness=(0.9, 1.0), p=1)
+                ], p=1),
+                albu.Downscale(scale_min=0.5, scale_max=0.5, interpolation=cv2.INTER_AREA, p=1)
+            ],
+            p=1
+        ),
+        albu.OneOf(
+            [
+                albu.IAAAdditiveGaussianNoise(p=1),
+                albu.GaussNoise(var_limit=(5.0, 25.0), p=1),
+                albu.ImageCompression(quality_lower=98, p=1)
+            ],
+            p=1
+        )
     ])
 
 
