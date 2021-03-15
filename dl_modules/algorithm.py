0113e09d433f4638f85132d7aedc469c61baa036
xiyori <foma@shipilov.ru>
2021-03-15 14:44:27 +0800

    Change GAN loss coeff

--- dl_modules/algorithm.py
+++ dl_modules/algorithm.py
@@ -10,7 +10,7 @@ from dl_modules.loss import VGGPerceptual, LSGANGenLoss, \
 gen_opt_state_dict = None
 dis_opt_state_dict = None
 
-gan_loss_coeff = 0.1
+gan_loss_coeff = 0.05
 init_gen_lr = 0.001
 dis_lr = 0.0001
 
